05fe72f6ed10095a8d2bcecdcdba2e820381815b
097MohamatFuatHasan <149357118+097MohamatFuatHasan@users.noreply.github.com>
2023-11-01 23:04:39 +0700

    Update sourceCode.py

--- tugasPertemuan9/sourceCode.py
+++ tugasPertemuan9/sourceCode.py
@@ -1,1 +1,49 @@
-print ('Hello World')
+# Daftar menu pizza
+menu_pizza = {
+    #Jenis pilihan Pizza yang tersedia
+    "Frankfurter BBQ": {
+        #Jenis Crust
+        "Pan": {
+            #Jenis Ukuran
+            "Personal": 43637,
+            "Regular": 100910,
+            "Large": 132728
+        },
+        "StuffedCrust Cheese": {
+            "Personal": 55455,
+            "Regular": 120910,
+            "Large": 160000
+        },
+        "StuffedCrust Sausage": {
+            "Personal": 52728,
+            "Regular": 117273,
+            "Large": 155455
+        },
+        "Cheese Bites": {
+            "Personal": 57273,
+            "Regular": 123637,
+            "Large": 164546
+        },
+        "Crowncrust": {
+            "Personal": 55455,
+            "Regular": 120910,
+            "Large": 160000
+        }
+    },
+
+    "Meat Monsta": {
+        "Pan": {
+            "Personal": 43637,
+            "Regular": 100910,
+            "Large": 132728
+        },
+              "StuffedCrust Cheese": {
+            "Personal": 55455,
+            "Regular": 120910,
+            "Large": 160000
+        },
+        "StuffedCrust Cheese": {
+            "Personal": 55455,
+            "Regular": 120910,
+            "Large": 160000
+        },
