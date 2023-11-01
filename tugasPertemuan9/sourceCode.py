73c5883d304e223551c37cae46c6c97db73fb0b2
MUlilAMrI23091397091 <149356238+MUlilAMrI23091397091@users.noreply.github.com>
2023-11-02 00:44:07 +0700

    Update sourceCode.py

--- tugasPertemuan9/sourceCode.py
+++ tugasPertemuan9/sourceCode.py
@@ -41,4 +41,55 @@ menu_pizza = {
             "Personal": 55455,
             "Regular": 120910,
             "Large": 160000
+         },
+        "StuffedCrust Sausage": {
+            "Personal": 52728,
+            "Regular": 117273,
+            "Large": 155455
+        },
+        "Cheese Bites": {
+            "Personal": 57273,
+            "Regular": 123637,
+            "Large": 164546
+        },
+        "Crowncrust": {
+            "Personal": 55455,
+            "Regular": 120910,
+            "Large": 160000
+        }
+    },
+
+    "Super Supreme": {
+        "Pan": {
+            "Personal": 43637,
+            "Regular": 100910,
+            "Large": 132728
+        },
+        "StuffedCrust Cheese": {
+            "Personal": 55455,
+            "Regular": 120910,
+            "Large": 160000
+        },
+        "StuffedCrust Sausage": {
+            "Personal": 52728,
+            "Regular": 117273,
+            "Large": 155455
+        },
+        "Cheese Bites": {
+            "Personal": 57273,
+            "Regular": 123637,
+            "Large": 164546
+        },
+        "Crowncrust": {
+            "Personal": 55455,
+            "Regular": 120910,
+            "Large": 160000
+        }
+    },
+
+    "Super Supreme Chicken": {
+        "Pan": {
+            "Personal": 43637,
+            "Regular": 100910,
+            "Large": 132728
         },
